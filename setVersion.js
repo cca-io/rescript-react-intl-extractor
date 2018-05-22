eeb8cac27f0cf7b3f42eaff7b383257bac50c64d
Christoph Knittel <christoph.knittel@cca.io>
2018-05-22 16:12:07 +0200

    Command line args via Arg module; -v to show version.

---
+++ setVersion.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const { version } = require("./package.json");
+
+fs.writeFileSync("bin/Version.re", `let version = "${version}";`);
+
+console.log(`Version set to ${version}`);
