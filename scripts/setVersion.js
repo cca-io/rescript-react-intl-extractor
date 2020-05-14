550e8706beba990a58805383d22c0405953168d5
Christoph Knittel <christoph.knittel@cca.io>
2020-05-14 08:11:58 +0200

    Fix "esy format"

--- scripts/setVersion.js
+++ scripts/setVersion.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const { version } = require("../package.json");
 
-fs.writeFileSync("bin/Version.re", `let version = "${version}";`);
+fs.writeFileSync("bin/Version.re", `let version = "${version}";\n`);
 
 console.log(`Version set to ${version}`);
