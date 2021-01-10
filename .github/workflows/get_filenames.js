07400fbc14c8df444df227c00366b7ba9f62d90a
Christoph Knittel <christoph.knittel@cca.io>
2021-01-10 15:58:02 +0100

    Fix artifact filenames (#67)

---
+++ .github/workflows/get_filenames.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const os = require("os");
+
+const packageJson = fs.readFileSync("package.json");
+const { version } = JSON.parse(packageJson);
+
+const exeName = getExeName();
+const platform = getPlatformName();
+const artifactName = `bs-react-intl-extractor-${version}-${platform}`;
+
+// For passing output to subsequent GitHub actions
+console.log(`::set-output name=exe_name::${exeName}`);
+console.log(`::set-output name=artifact_name::${artifactName}`);
+
+function getPlatformName() {
+  const platform = os.platform();
+
+  switch (platform) {
+    case "linux":
+      return "linux";
+    case "darwin":
+      return "macos";
+    case "win32":
+      return "windows";
+    default:
+      throw new Error(`Unexepected platform ${platform}`);
+  }
+}
+
+function getExeName() {
+  const platform = os.platform();
+
+  switch (platform) {
+    case "win32":
+      return "bs-react-intl-extractor.exe";
+    default:
+      return "bs-react-intl-extractor";
+  }
+}
